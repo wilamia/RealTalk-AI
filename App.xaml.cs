6a6c90a56d430c697bb1f1c351223c40e62ea65c
Kseniya <eseniya.wi@gmail.com>
2025-02-27 17:54:53 +0300

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace RealTalk_AI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
